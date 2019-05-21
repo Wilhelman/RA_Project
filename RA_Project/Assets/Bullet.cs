3f6399550de82ff4ed0b670fae5ee7587fff87e0
Wilhelman <guillermogarsub@gmail.com>
2019-05-21 18:03:18 +0200

    collisions added

--- RA_Project/Assets/Bullet.cs
+++ RA_Project/Assets/Bullet.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
     public float velocity = 1.0f;
+    public GameObject goal;
+    public GameObject goalkeeper;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.forward * velocity);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == goal)
+        {
+            // 1 POINT TO GRIFFINDOR
+            Destroy(this);
+        }else if( collision.gameObject == goalkeeper)
+        {
+            // 1 POINT TO SLICERING
+            Destroy(this);
+        }
+
+
     }
 }
