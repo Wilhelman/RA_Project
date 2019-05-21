5fcbc053b1019468cd3c6b0ee0dcad8bf86492a8
Wilhelman <guillermogarsub@gmail.com>
2019-05-21 11:37:52 +0200

    Added script to spawn zombies in certain area

---
+++ RA_Project/Assets/Spawner3000.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner3000 : MonoBehaviour {
+
+    public GameObject zombie_prefab = null;
+
+    public float spawn_timer = 3.0f;
+
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer > spawn_timer)
+        {
+            timer = 0.0f;
+            float x_pos = Random.Range(-5.0f, 5.0f);
+            float z_pos = Random.Range(-5.0f, 5.0f);
+            Vector3 random_pos = new Vector3(transform.position.x + x_pos, transform.position.y, transform.position.z + z_pos);
+            Instantiate(zombie_prefab, random_pos, Quaternion.identity);
+        }
+	}
+}
