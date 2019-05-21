8a9dd2114b36df7214021062cac696da3c2204ee
Wilhelman <guillermogarsub@gmail.com>
2019-05-21 17:59:56 +0200

    implemented bullet script

---
+++ RA_Project/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float velocity = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * velocity);
+    }
+}
