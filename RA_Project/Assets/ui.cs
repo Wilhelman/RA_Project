a901b6800f0250f9fe87132e065ef2bf044e3972
Wilhelman <guillermogarsub@gmail.com>
2019-05-21 22:21:26 +0200

    implement ui script

---
+++ RA_Project/Assets/ui.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui : MonoBehaviour {
+
+    public Text thing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            thing.text = "one pressed";
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            thing.text = "two pressed";
+    }
+}
