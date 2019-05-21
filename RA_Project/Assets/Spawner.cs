b8bc45cd1cd2bb780ad9dac2405063706e8b86e0
nintervik <vmasogarcia@gmail.com>
2019-05-21 14:17:08 +0200

    spawn controlled

--- RA_Project/Assets/Spawner.cs
+++ RA_Project/Assets/Spawner.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject turret_target = null;
 
-    public GameObject zombie_prefab = null;
-
-    public float spawn_timer = 3.0f;
+    public float spawn_timer = 5.0f;
+    public float zombie_speed = 0.01f;
 
     private float timer = 0.0f;
+
 
     public GameObject[] zombies;
     private int count = 0;
 
     // Use this for initialization
-    void Start() {
-
+    void Start()
+    {
         foreach (GameObject z in zombies)
             z.SetActive(false);
     }
@@ -31,6 +32,23 @@ public class Spawner : MonoBehaviour
             zombies[count].SetActive(true);
             timer = 0.0f;
             count++;
+        }
+
+        foreach (GameObject z in zombies)
+        {
+            if (z.active == true)
+            {
+                Vector3 dir = turret_target.transform.position - z.transform.position;
+                dir.Normalize();
+                z.transform.position += zombie_speed * dir;
+            }
+
+            if (Mathf.Abs((z.transform.position - turret_target.transform.position).magnitude) <= 3.0f)
+            {
+                z.transform.position = this.transform.position;
+                z.SetActive(false);
+            }
+                
         }
 
         // TODO: if all zombies are dead start again
